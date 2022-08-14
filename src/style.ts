15aed8270fc9e32e7b2d09b0775cf35b7aeef571
sakkke <w32w64@gmail.com>
2022-08-14 12:24:00 +0900

    :lipstick: add styles

---
+++ src/style.ts
@@ -0,0 +1,11 @@
+// common variables
+import 'open-props/style'
+// initialize styles
+import 'open-props/normalize'
+
+// catppuccin variables
+import '@catppuccin/palette/style'
+// light theme
+import './styles/latte.css'
+// dark theme
+import './styles/macchiato.css'
